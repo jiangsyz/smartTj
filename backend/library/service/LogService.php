74d7dac8d78cf5ee819248ebd7e3e316f9457b06
fengjiesam <chaomengfj@qq.com>
2018-07-12 09:57:06 +0800

    统计系统

--- backend/library/service/LogService.php
+++ backend/library/service/LogService.php
@@ -33,9 +33,8 @@ class LogService
             if (empty($sub_order_ids)) {
                 throw new \Exception('sub_pay_order is empty');
             }
-
             $list = Yii::$app->db->createCommand('SELECT sourceId,SUM(finalPrice*buyingCount) as price, SUM(buyingCount) as buy_count
-              FROM order_buying_record where sourceType = 2 GROUP BY sourceId ORDER BY price DESC
+              FROM order_buying_record where sourceType = 2 AND orderId IN ('.implode(', ', $sub_order_ids).') GROUP BY sourceId ORDER BY price DESC
             ')->queryAll();
             if (empty($list)) {
                 throw new \Exception('data is null');
