2488278d79d7affde80e663be5272e0487d5d60c
fengjiesam <chaomengfj@qq.com>
2018-07-18 17:18:14 +0800

    统计管理

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -18,9 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <?= $form->field($model, 'password')->passwordInput() ?> 
 
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
